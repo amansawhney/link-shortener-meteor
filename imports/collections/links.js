8f7559e3c80eb457afea0b54930db373a9e49cc6
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-29 11:19:44 -0400

    Added a insert link method

--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,3 +1,9 @@
 import { Mongo } from 'meteor/mongo';
 
+Meteor.methods({
+  'links.insert': function() {  //wraping a . makes a key vaild
+    
+  }
+});
+
 export const Links = new Mongo.Collection('links');
