d0e4ef1168225fb6a89988ac3f0c00cd5ea1d47b
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-29 11:49:50 -0400

    Added Validation for urls

--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,8 +1,14 @@
 import { Mongo } from 'meteor/mongo';
+import vaildUrl from 'valid-url';
+import { check, Match } from 'meteor/check'
 
 Meteor.methods({
   'links.insert': function(url) {  //wraping a . makes a key vaild
-    url += '.com'
+    url += '.com';
+    if (!/^(f|ht)tps?:\/\//i.test(url)) {
+      url = "http://" + url;
+    }
+    check(url, Match.Where(url => vaildUrl.isUri(url)));
     console.log('attempting to save', url);
   }
 });
