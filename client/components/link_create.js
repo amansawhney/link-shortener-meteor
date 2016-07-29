c77ad1cc266a24454e14080093e5d92bd4c16e5d
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-29 11:39:08 -0400

    Excuted Meteor method on the client

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -3,19 +3,19 @@ import React, { Component } from 'react';
 class LinkCreate extends Component {
   handleSubmit (event) {
     event.preventDefault();
-
     console.log(this.refs.input.value);
-
+    Meteor.call('links.insert', this.refs.input.value);
   }
 
   render() {
     return (
       <div className = "col-md-6 well col-md-offset-3">
         <form onSubmit={this.handleSubmit.bind(this)}>
-            <div className="form-group">
-              <label> Link to Shorten </label>
-              <input ref='input' className='form-control' />
+            <div className="input-group">
+              <input ref='input' type="text" className="form-control" placeholder="Link To Shorten" aria-describedby="basic-addon2" />
+              <span className="input-group-addon" id="basic-addon2">.com</span>
             </div>
+            <br />
             <button className = "btn btn-primary"> SHORTEN </button>
         </form>
       </div>
--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,8 +1,9 @@
 import { Mongo } from 'meteor/mongo';
 
 Meteor.methods({
-  'links.insert': function() {  //wraping a . makes a key vaild
-    
+  'links.insert': function(url) {  //wraping a . makes a key vaild
+    url += '.com'
+    console.log('attempting to save', url);
   }
 });
 
