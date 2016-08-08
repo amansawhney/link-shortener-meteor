7534ad5c2f6d06171ef14e62e11dd83ccbaf1494
amansawhney <amansawhney@sawhneygames.com>
2016-08-08 17:20:02 -0400

    Added Boilerplate for linklist coponent

---
+++ client/components/link_list.js
@@ -0,0 +1,11 @@
+import React, {Componenet} from 'react';
+
+class LinkList extends Componenet {
+  render() {
+    return (
+      <div>List of Links to click</div>
+    );
+  }
+}
+
+export default LinkList;
